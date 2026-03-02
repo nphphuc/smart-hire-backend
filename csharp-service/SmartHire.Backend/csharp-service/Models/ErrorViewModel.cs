fd94ecfe0d005518018809389b676ee480c386ff
phatom2005 <khoinguyen.vo910@gmail.com>
2026-03-02 21:04:58 +0700

    .

--- csharp-service/SmartHire.Backend/csharp-service/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace csharp_service.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
