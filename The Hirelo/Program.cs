a7af9c9844ffa6dd97dbe5e33de27e5a5f54572b
nphphuc <pavilionforc@gmail.com>
2026-03-19 23:23:26 +0700

    fix error 500

--- The Hirelo/Program.cs
+++ The Hirelo/Program.cs
@@ -211,6 +211,8 @@ namespace The_Hirelo
             //builder.WebHost.UseUrls("http://0.0.0.0:8080");
             // Day la lop bao ve de API chay duoc tren moi truong Lambda
             builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+            // DI Container for CVService
+            builder.Services.AddScoped<ICVService, CVService>();
 
             var app = builder.Build();
 
